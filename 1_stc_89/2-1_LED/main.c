2adba71aba7772730d884102fc21217bfb6fdc0b
ychinata <ychinata@qq.com>
2022-09-27 11:38:35 +0800

    No.: STC89_DEMO_1_LED Author: xy Description: STC89点灯

--- 1_stc_89/2-1_LED/main.c
+++ 1_stc_89/2-1_LED/main.c
@@ -1,9 +1,28 @@
 #include <REGX52.H>
 
+// 声明
+void Delay1ms(unsigned int xms);
+
+
 void main()
 {
-	P2 = 0xFE;	//1111 1110
-	while(1) {
-		
+    while(1) {
+        P2 = 0xFF;	//共阳,全灭
+        Delay1ms(500);
+        P2 = 0x00;  // 共阳,全亮
+        Delay1ms(500);
+	}
+}
+
+void Delay1ms(unsigned int xms)		//@12.000MHz
+{
+	unsigned char i, j;
+	while(xms) {
+		i = 2;
+		j = 239;
+		do {
+			while (--j);
+		} while (--i);
+		xms--;
 	}
 }
